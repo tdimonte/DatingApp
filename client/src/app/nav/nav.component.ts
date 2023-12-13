49e77d44421b55854c2d9e5692a5b2251fb17f43
Thomas DiMonte <tdimonte@nvisia.com>
2023-12-13 15:37:09 -0600

    Added login, home page, register, account service

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { User } from '../_models/user';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {}
+
+  constructor(public accountService: AccountService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+}
+function Of(arg0: null): Observable<User | null> {
+  throw new Error('Function not implemented.');
+}
+
